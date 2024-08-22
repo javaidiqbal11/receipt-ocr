c2764f0eee3657bd85cab0285efc3355dfdb21bf
Javaid Iqbal <javaid.ciit@gmail.com>
2024-08-22 18:34:58 +0500

    Update yolov8_train.py

--- src/yolov8_train.py
+++ src/yolov8_train.py
@@ -1,26 +1,21 @@
 from ultralytics import YOLO
-import os
 
-from ultralytics.data.explorer.explorer import Explorer
-
-
-def train_yolov8_model(data_dir, model_save_path):
+def train_yolov8_model(data_yaml, model_save_path):
     # Load YOLOv8 model (either a pre-trained one or a custom architecture)
-    model = YOLO('yolov8n.yaml')  # Load a YOLOv8 nano model configuration for training
+    model = YOLO('yolov8m.pt')  # Load a YOLOv8 medium pre-trained model (recommended for training)
     
     # Set the training parameters
-    model.train(data=data_dir, epochs=50, imgsz=640, batch=16, name='yolov8_receipt_detection')
+    model.train(data=data_yaml, epochs=50, imgsz=1024, batch=16, name='yolov8_receipt_detection')
     
     # Save the trained model
     model.export(format="torchscript", path=model_save_path)
 
 if __name__ == "__main__":
-    data_dir = {
-        'train': '../data/images',  # Path to training images
-        'val': '../data/images',    # Path to validation images (can be the same as train if split not provided)
-        'names': '../data/classes.txt',  # Path to the classes file
-        'labels': '../data/labels'  # Path to the labels
-    }
+    # Path to the dataset YAML file
+    data_yaml = 'data/dataset.yaml'  # Ensure this points to your dataset.yaml file
     
-    model_save_path = '../models/yolov8_receipt_detection.pt'
-    train_yolov8_model(data_dir, model_save_path)
+    # Path where the trained model will be saved
+    model_save_path = 'models/yolov8_receipt_detection.pt'
+    
+    # Train the YOLOv8 model
+    train_yolov8_model(data_yaml, model_save_path)
